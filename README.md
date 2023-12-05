4b1f2361d26b51b3adad1e0732572346231a77da
BladeTC <48643865+BladeTC@users.noreply.github.com>
2023-12-05 19:33:48 +0200

    read user unput and infinite scycle

--- README.md
+++ README.md
@@ -10,12 +10,22 @@ npm i
 
 # run project
 
-````
+```
 
 node index.js
 
 ```
 
-[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/BladeTC/snace-js)
+# run in dev mode
+
 ```
-````
+
+npm run dev
+
+```
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/BladeTC/snace-js)
+
+```
+
+```
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // run `node index.js` in the terminal
+var readlineSync = require("readline-sync");
 
 let worldSize = 15;
 const food = "*";
@@ -12,33 +13,35 @@ let playerPosition = {
   y: 7,
 };
 let row = "";
-
-for (let y = 0; y < worldSize; y++) {
-  for (let x = 0; x < worldSize; x++) {
-    let foundBody = false;
-    for (let bodyPosition of bodyPositions) {
-      if (bodyPosition.x == x && bodyPosition.y == y) {
-        row += body;
-        foundBody = true;
+do {
+  console.clear();
+  for (let y = 0; y < worldSize; y++) {
+    for (let x = 0; x < worldSize; x++) {
+      let foundBody = false;
+      for (let bodyPosition of bodyPositions) {
+        if (bodyPosition.x == x && bodyPosition.y == y) {
+          row += body;
+          foundBody = true;
+        }
       }
-    }
-    if (foundBody) {
-      continue;
-    }
-    if (playerPosition.x == x && playerPosition.y == y) {
-      row += player;
-      if (direction == "w") {
-        player.y++;
+      if (foundBody) {
+        continue;
       }
-    } else row += "-";
+      if (playerPosition.x == x && playerPosition.y == y) {
+        row += player;
+        if (direction == "w") {
+          player.y++;
+        }
+      } else row += "-";
+    }
+    console.log(row);
+    row = "";
   }
-  console.log(row);
-  row = "";
-}
-//return console.log("0");
+  direction = readlineSync.question();
+} while (direction != "a");
+// //return console.log("0");
 
-process.stdin.on("data", (data) => {
-  // stdin(direction);
-  direction = data.toString();
-  console.log(direction);
-});
+// process.stdin.on("data", (data) => {
+//   direction = data.toString();
+//   console.log(direction);
+// });
